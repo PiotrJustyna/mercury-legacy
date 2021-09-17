3d6614a699d0f44d0d59550d0473fd02fdd06d61
Piotr Justyna <piotr.justyna@gmail.com>
2021-09-17 09:52:36 +0100

    Working solution.

---
+++ MercuryLegacy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MercuryLegacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
